7ed0fe2af7f38ef6318eac865ac7b55bb7404cf0
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-29 15:41:30 +0200

    [UPD] MySQL router working in App!

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
+const moviesRouter = require('./routers/moviesRouter');
+
 app.use(express.static('public'));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send('Movies API Server')
+});
+
+app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
